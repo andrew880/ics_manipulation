b07af4f6f61113de1ec9fd99667ba7d7d1b4f589
Andrew Lin <allin5@illinois.edu>
2020-06-15 14:38:21 -0700

    delete debuging messages

--- cal_shift.py
+++ cal_shift.py
@@ -12,22 +12,15 @@ try:
 except Exception as e:
     print("incorrect format, run program again")
 
-
 if len(list(c.events)) > 0:
     first_event_date = list(c.events)[0].begin
 for event in list(c.events):
-    print(first_event_date)
-    print(event.begin)
     if event.begin - first_event_date <= timedelta(0):
         first_event_date = event.begin
 datedif = start_date - arrow.get(first_event_date.year, first_event_date.month, first_event_date.day)
-print(datedif)
 
 events = set()
 for event in c.events:
-    print('---------')
-    print(event.begin)
-    print(event.end)
     if datedif > timedelta(0):
         event.end += datedif
         event.begin += datedif
@@ -36,12 +29,7 @@ for event in c.events:
         event.end += datedif
     if arrow.get(event.end.year, event.end.month, event.end.day) == event.end and arrow.get(event.begin.year, event.begin.month, event.begin.day) == event.begin:
         event.end -= timedelta(days = 1) #to accomodate rounding error from make_all_day
-        print(event.begin)
-        print(event.end)
-        print('allday')
         event.make_all_day()
-    print(event.begin)
-    print(event.end)
     events.add(event)
 c = ics.Calendar(imports = None, events = events, todos = c.todos, creator = c.creator)
 
